7c97e97b21f7aa3e6540c7382ce6b13dda94342f
sampaio-arthur <arthurbarrasampaio@gmail.com>
2024-08-08 15:30:49 -0300

    ResponseEntity

--- src/main/java/br/com/sampaioarthur/TodoList/user/UserController.java
+++ src/main/java/br/com/sampaioarthur/TodoList/user/UserController.java
@@ -1,6 +1,8 @@
 package br.com.sampaioarthur.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,16 +21,18 @@ public class UserController {
 
     // body da requisicao
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
         if(user != null){
             System.out.println("Usuário existente!");
-            return null;
+            // mensagem de erro
+            // status code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
         }
 
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
 
     }
     
