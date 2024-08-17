d49896004ccbba8dd49eae2a36caf99532294748
sampaio-arthur <arthurbarrasampaio@gmail.com>
2024-08-17 11:24:31 -0300

    Implementação

--- src/main/java/br/com/sampaioarthur/TodoList/filter/FilterTaskAuth.java
+++ src/main/java/br/com/sampaioarthur/TodoList/filter/FilterTaskAuth.java
@@ -1,27 +1,47 @@
 package br.com.sampaioarthur.todolist.filter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 
 @Component
-public class FilterTaskAuth implements Filter {
-    
-    
+public class FilterTaskAuth extends OncePerRequestFilter {
+
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
-        throws IOException, ServletException{
-        // executar alguma acao
-    
-        System.out.println("Chegou no filtro");    
-        chain.doFilter(request, response);  
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        
+               var authorization = request.getHeader("Authorization");
+              
+               
+               var authEncoded = authorization.substring("Basic" .length()).trim();
+
+               byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+
+               var authString = new String(authDecoded);
+
+               String[] credencials = authString.split(":");
+               String username = credencials[0];
+               String password = credencials[1];
+               System.out.println("authorization");
+               System.out.println(username);
+               System.out.println(password);
+
+
+                filterChain.doFilter(request, response);
+
     }
+    
+    
+   
 
 }
