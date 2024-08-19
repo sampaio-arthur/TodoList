5625339c2d7807fca9f68ff36b243e39eb3e1f23
sampaio-arthur <arthurbarrasampaio@gmail.com>
2024-08-19 20:00:00 -0300

    Setando atrinutos

--- src/main/java/br/com/sampaioarthur/TodoList/task/TaskController.java
+++ src/main/java/br/com/sampaioarthur/TodoList/task/TaskController.java
@@ -1,10 +1,15 @@
 package br.com.sampaioarthur.todolist.task;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -15,8 +20,10 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel){
+    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request){
         System.out.println("chegou no controller");
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID)idUser);
         var task = this.taskRepository.save(taskModel);
         return task;
     } 
